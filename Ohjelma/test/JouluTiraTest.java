afce83fab8614feb57663d0a89535e39a13b1edc
Cobrelli <Cobrelli@192.168.1.34>
2012-12-11 17:31:26 +0200

    ekat testit valmiina, nyt lukemaan otm tenttiin ^^

--- Ohjelma/test/JouluTiraTest.java
+++ Ohjelma/test/JouluTiraTest.java
@@ -6,29 +6,43 @@ import org.junit.Test;
 
 public class JouluTiraTest {
     
+    Sokkelo s;
     
     public JouluTiraTest() {
     }
         
     @Before
     public void setUp() {
+        s = new Sokkelo();
     }
     
-    @Test
-    public void testaaTulostus(){
-        char[][] s = new char[][] {{'0'}};
-        
-        assertEquals(s[0][0], '0');
-    }
     @After
     public void tearDown() {
     }
     
     @Test
-    public void testaaPaikka(){
+    public void testaaEttaAnnettuPaikkaOnOikea(){
         
         Paikka p = new Paikka(1, 1);
-        
+        assertEquals(1, p.getX());
         assertEquals(1, p.getY());
+    }
+    
+    @Test
+    public void testaaEttaGeneroituSokkeloEiTuotaSeiniaAlkuun(){
+        int[][] t = s.teeSokkelo(5, 5);
+        assertEquals(0, t[0][0]);
+    }
+    
+    @Test
+    public void testaaEttaGeneroituSokkeloEiTuotaSeiniaLoppuun(){
+        int[][] t = s.teeSokkelo(5, 5);
+        assertEquals(0, t[4][4]);
+    }
+    
+    //tee testi joka tarkastaa että sokkelo on mahdollista läpäistä
+    @Test
+    public void testaaEttaSokkeloOnMahdollistaLapaista(){
+        
     }
 }
