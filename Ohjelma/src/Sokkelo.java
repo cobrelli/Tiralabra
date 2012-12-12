29b49e4581fac18985a154cad78dc341bddf0f9f
Cobrelli <Cobrelli@192.168.1.34>
2012-12-12 21:23:37 +0200

    javadocceja sokkelolle

--- Ohjelma/src/Sokkelo.java
+++ Ohjelma/src/Sokkelo.java
@@ -2,7 +2,13 @@
 import java.util.Random;
 
 /**
- *
+ * Luo uuden luokan Sokkelo, joka tarvittaessa generoi matriisi -sokkeloita.
+ * Sokkelot luokaan looppaamalla ja jokaisen kohdalla arvotaan satunnaisesti luku
+ * välillä 0-1, jos luku on 0.3 tai alle niin kohdalle asetetaan seinä. Seiniä ei
+ * luoda aloitus ja lopetuspaikkoihin, jotka sijaisevat alku, vasen ylänurkka ja loppu,
+ * oikea alanurkka. Tämän jälkeen tarkastetaan syvyyssuuntaisella haulla onko 
+ * saatu sokkelo mahdollista yleensä läpäistä. Jos ei niin generoidaan uusia niin
+ * kauan että sokkelo on läpäistävissä.
  * @author Cobrelli
  */
 public class Sokkelo {
@@ -11,7 +17,7 @@ public class Sokkelo {
     boolean onkoLapaistavissa;
 
     /**
-     *
+     *  Konstruktori alustaa Randomin
      */
     public Sokkelo() {
         r = new Random();
@@ -19,10 +25,14 @@ public class Sokkelo {
 
     
     /**
-     *
-     * @param korkeus
-     * @param leveys
-     * @return
+     * Luo uuden halutunkorkuisen ja levyisen sokkelon
+     * generoi sokkelon ja testattavan sokkelon, ja kutsuu sitten testaaSokkeloa, joka
+     * tarkistaa onko se yleensä mahdollista ratkaista, jos ei niin generoi uuden
+     * sokkelon niin kauan kunnes sokkelo on ratkaistavissa.
+     * 
+     * @param korkeus   kertoo matriisin korkeuden
+     * @param leveys    kertoo matriisin leveys
+     * @return          palauttaa ratkaistavissa olevan sokkelon matriisina
      */
     public int[][] teeSokkelo(int korkeus, int leveys) {
         int[][] sokkelo = new int[korkeus][leveys];
@@ -40,14 +50,16 @@ public class Sokkelo {
         return sokkelo;
     }
 
-    //testaa DFS:llä että annettu sokkelo on mahdollista läpäistä
-    
     /**
-     *
-     * @param sokkelo
-     * @param y
-     * @param x
-     * @return
+     * Testaa DFS:llä että annettu sokkelo on mahdollista läpäistä. Jos on 
+     * saavutettu maalipiste niin sokkelo on ratkaistavissa ja muutetaan globaali
+     * muuttuja true arvoon, muuten palautetaan false. Käyttää
+     * validien liikkeiden määrittelyyn apumetodia onkoValidi.
+     * 
+     * @param sokkelo   on testattava aiemmin generoitu sokkelo
+     * @param y         on sen hetkinen akselin Y paikka taulukossa, alkaa 0.
+     * @param x         on sen hetkinen akselin X paikka taulukossa, alkaa 0.
+     * @return          Palauttaa true tai false riippuen ratkaistavuudesta    
      */
     public boolean testaaSokkelo(int[][] sokkelo, int y, int x) {
 
@@ -75,13 +87,15 @@ public class Sokkelo {
         return false;
     }
 
-    //tarkistaa onko siirtyminen mahdollista
     /**
-     *
-     * @param s
-     * @param y
-     * @param x
-     * @return
+     * Tarkistaa onko siirtyminen validi liike. Ei valideja liikkeitä ovat
+     * matriisin reunojen ylitse menevät siirtymiset tai jos paikka sisältää
+     * seinän.
+     * 
+     * @param s     taulukko, jota tarkastellaan
+     * @param y     paikka Y akselilla johon halutaan liikkua
+     * @param x     paikka X akselilla johon halutaan liikkua
+     * @return      palauttaa false jos ei validi, palauttaa true jos validi.
      */
     public boolean onkoValidi(int[][] s, int y, int x) {
         if (y > s.length - 1 || y < 0 || x < 0 || x > s[0].length - 1 || s[y][x] == 1) {
