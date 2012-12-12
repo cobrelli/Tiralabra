354679b098addced0c8876fee39893d4985a12dd
Cobrelli <Cobrelli@192.168.1.34>
2012-12-12 14:05:13 +0200

    Sokkeloluokan refaktorointia

--- Ohjelma/src/Sokkelo.java
+++ Ohjelma/src/Sokkelo.java
@@ -1,42 +1,39 @@
 
 import java.util.Random;
 
+/**
+ *
+ * @author Cobrelli
+ */
 public class Sokkelo {
 
     Random r;
-    boolean b;
+    boolean onkoLapaistavissa;
 
+    /**
+     *
+     */
     public Sokkelo() {
         r = new Random();
     }
 
+    
+    /**
+     *
+     * @param korkeus
+     * @param leveys
+     * @return
+     */
     public int[][] teeSokkelo(int korkeus, int leveys) {
         int[][] sokkelo = new int[korkeus][leveys];
         int[][] testattava = new int[korkeus][leveys];
+        generoiSokkelo(korkeus, leveys, sokkelo, testattava);
 
-        for (int i = 0; i < korkeus; i++) {
-            for (int n = 0; n < leveys; n++) {
-                double luku = r.nextDouble();
-                if (i == 0 && n == 0 || i == korkeus - 1 && n == leveys - 1) {
-                    sokkelo[i][n] = 0;
-                    testattava[i][n] = 0;
-                } else {
-                    if (luku <= 0.3) {
-                        sokkelo[i][n] = 1;
-                        testattava[i][n] = 1;
-                    } else {
-                        sokkelo[i][n] = 0;
-                        testattava[i][n] = 0;
-                    }
-                }
-            }
-        }
-
-        b = false;
+        onkoLapaistavissa = false;
 
         testaaSokkelo(testattava, 0, 0);
 
-        if (!b) {
+        if (!onkoLapaistavissa) {
             return teeSokkelo(korkeus, leveys);
         }
 
@@ -44,10 +41,18 @@ public class Sokkelo {
     }
 
     //testaa DFS:llä että annettu sokkelo on mahdollista läpäistä
+    
+    /**
+     *
+     * @param sokkelo
+     * @param y
+     * @param x
+     * @return
+     */
     public boolean testaaSokkelo(int[][] sokkelo, int y, int x) {
 
         if (y == sokkelo.length - 1 && x == sokkelo[0].length - 1) {
-            b = true;
+            onkoLapaistavissa = true;
             return true;
         }
 
@@ -71,10 +76,44 @@ public class Sokkelo {
     }
 
     //tarkistaa onko siirtyminen mahdollista
+    /**
+     *
+     * @param s
+     * @param y
+     * @param x
+     * @return
+     */
     public boolean onkoValidi(int[][] s, int y, int x) {
         if (y > s.length - 1 || y < 0 || x < 0 || x > s[0].length - 1 || s[y][x] == 1) {
             return false;
         }
         return true;
+    }
+
+    /**
+     *
+     * @param korkeus
+     * @param leveys
+     * @param sokkelo
+     * @param testattava
+     */
+    public void generoiSokkelo(int korkeus, int leveys, int[][] sokkelo, int[][] testattava) {
+        for (int i = 0; i < korkeus; i++) {
+            for (int n = 0; n < leveys; n++) {
+                double luku = r.nextDouble();
+                if (i == 0 && n == 0 || i == korkeus - 1 && n == leveys - 1) {
+                    sokkelo[i][n] = 0;
+                    testattava[i][n] = 0;
+                } else {
+                    if (luku <= 0.3) {
+                        sokkelo[i][n] = 1;
+                        testattava[i][n] = 1;
+                    } else {
+                        sokkelo[i][n] = 0;
+                        testattava[i][n] = 0;
+                    }
+                }
+            }
+        }
     }
 }
