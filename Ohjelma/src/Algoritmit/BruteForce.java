4036539c3f83723e4423264762b12fa8a42d413f
Cobrelli <Cobrelli@192.168.1.34>
2012-12-15 23:39:37 +0200

    javadocseja

--- Ohjelma/src/Algoritmit/BruteForce.java
+++ Ohjelma/src/Algoritmit/BruteForce.java
@@ -1,13 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Algoritmit;
 
 import java.util.Random;
 
 /**
- *
+ * BruteForce menetelmällä sokkelon läpikäyntiä. Tarkoituksena olla mahdollisimman
+ * tehoton, jotta saadaan vertailukohtaa oikeisiin pathfinding algoritmeihin.
  * @author Cobrelli
  */
 public class BruteForce {
@@ -20,10 +17,16 @@ public class BruteForce {
     }
 
     /**
-     *
-     * @param sokkelo
-     * @param random
-     * @return
+     * Ajaa bruteforcettamalla sokkelon lävitse. Ideana on satunnaisesti liikkua
+     * johonkin suuntaan ja tarkistaa onko siirto validi, jos ei niin seuraavalla
+     * kierrokselle haetaan uusi paikka. Oletuksena sokkelossa on että se on
+     * läpäistävissä, muuten se jää looppaamaan. Suoritus päättyy kun saavutetaan
+     * maaliruutu.
+     * 
+     * @param sokkelo       On sokkelo johon algoritmia lähdetään ajamaan.
+     * @param random        Random oliolla saadaan satunnaisuus mukaan, jolloin
+     *                      liikutaan satunnaisesti johonkin suuntaan.
+     * @return              Palauttaa reitin kokonaispituuden.
      */
     public int AjaBruteForce(int[][] sokkelo, Random random) {
         int kokonaisPituus = 0;
@@ -64,11 +67,11 @@ public class BruteForce {
     }
 
     /**
-     *
-     * @param s
-     * @param y
-     * @param x
-     * @return
+     * Siirtymisen validiuden tarkistamisessa käytettävä apumetodi.
+     * @param s     Sokkelo josta tarkistetaan paikkaa.
+     * @param y     Kertoo haettavan kohdan paikan y -akselilla.
+     * @param x     Kertoo haettavan kohdan paikan x -akselilla.
+     * @return      Jos siirto voidaan suorittaa palauttaa true, muuten false.
      */
     public boolean onkoSallittuLiike(int[][] s, int y, int x) {
         if (y > s.length - 1 || y < 0 || x < 0 || x > s[0].length - 1 || s[y][x] == 1) {
