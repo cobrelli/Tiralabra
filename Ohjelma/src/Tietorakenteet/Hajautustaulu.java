00f16c18f0fe5ce3188a572092c52675d3b25568
cobrelli <cobrelli@gmail.com>
2012-12-19 22:44:22 +0200

    hajautustaululuokka

---
+++ Ohjelma/src/Tietorakenteet/Hajautustaulu.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tietorakenteet;
+
+/**
+ *
+ * @author Cobrelli
+ */
+public class Hajautustaulu {
+    
+}
