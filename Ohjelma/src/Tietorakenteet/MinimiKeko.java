54fd67ea67dca4a23d9025a144d74aea6e2b5d8e
Cobrelli <Cobrelli@192.168.1.34>
2012-12-20 11:36:44 +0200

    kekoilua

--- Ohjelma/src/Tietorakenteet/MinimiKeko.java
+++ Ohjelma/src/Tietorakenteet/MinimiKeko.java
@@ -14,20 +14,64 @@ public class MinimiKeko {
 
     int heapSize;
     Paikka[] alkiot;
-    
+
     public MinimiKeko(int koko) {
         this.alkiot = new Paikka[koko];
-        this.heapSize = 0;
-    }
-    
-    public void minHeapify(){
-        
+        this.heapSize = -1;
     }
 
-    public int getLength(){
-        return alkiot.length;
+    public void heapInsert(Paikka p) {
+        heapSize++;
+        int i = heapSize;
+        while (i > 1 && alkiot[parent(i)].compareTo(p) > 0) {
+            alkiot[i] = alkiot[parent(i)];
+            i = parent(i);
+            alkiot[i] = p;
+        }
+    }
+
+    public Paikka heapDelMin() {
+        Paikka min = alkiot[0];
+        alkiot[0] = alkiot[heapSize];
+        heapSize--;
+        minHeapify(0);
+        return min;
+    }
+
+    public void minHeapify(int i) {
+//        Paikka l = alkiot[left(i)];
+//        Paikka r = alkiot[right(i)];
+
+        int l = left(i);
+        int r = right(i);
+        int pienin;
+
+        if (r <= heapSize) {
+            if (alkiot[l].compareTo(alkiot[r]) < 0) {
+                pienin = l;
+            } else {
+                pienin = r;
+            }
+            if (alkiot[i].compareTo(alkiot[pienin]) > 0) {
+                vaihda(i, pienin);
+                minHeapify(pienin);
+            }
+        } else if (l == heapSize && alkiot[i].compareTo(alkiot[l]) > 0) {
+            vaihda(i, l);
+        }
+    }
+
+    public void heapDecKey(int i, int uusiArvo){
+        if(uusiArvo<alkiot[i].getEtaisyys()){
+            alkiot[i].setEtaisyys(uusiArvo);
+            minHeapify(i);
+        }
     }
     
+    public int getLength() {
+        return alkiot.length;
+    }
+
     public int parent(int i) {
         return i / 2;
     }
@@ -38,5 +82,12 @@ public class MinimiKeko {
 
     public int right(int i) {
         return (2 * 1) + 1;
+    }
+
+    public void vaihda(int i, int pienin) {
+        //vaihda i ja pienin
+        Paikka vaihdettava = alkiot[i];
+        alkiot[i] = alkiot[pienin];
+        alkiot[pienin] = vaihdettava;
     }
 }
