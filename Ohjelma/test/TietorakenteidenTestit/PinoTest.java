01c55056567813c804fbdb17eb7bdd310167e7bd
Cobrelli <Cobrelli@192.168.1.34>
2012-12-20 22:53:39 +0200

    testejä

--- Ohjelma/test/TietorakenteidenTestit/PinoTest.java
+++ Ohjelma/test/TietorakenteidenTestit/PinoTest.java
@@ -4,6 +4,8 @@
  */
 package TietorakenteidenTestit;
 
+import Algoritmit.Paikka;
+import Tietorakenteet.Pino;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,28 +18,45 @@ import static org.junit.Assert.*;
  * @author Cobrelli
  */
 public class PinoTest {
-    
+
+    Pino pino;
+
     public PinoTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
     @Before
     public void setUp() {
+        pino = new Pino(10);
+    }
+
+    @Test
+    public void testaaEttaPalauttaaNullJosTyhjaJaPopataan() {
+        assertEquals(null, pino.pop());
+    }
+
+    @Test
+    public void testaaEttaPopPalauttaaPushatun() {
+        Paikka p = new Paikka(1, 1, 1);
+        pino.push(p);
+        assertEquals(p, pino.pop());
+    }
+
+    @Test
+    public void testaaEtteiTayteenPinoonLisaaminenAiheutaPoikkeusta() {
+        for (int i = 0; i < 20; i++) {
+            pino.push(new Paikka(1, 1, 1));
+        }
     }
     
-    @After
-    public void tearDown() {
+    @Test
+    public void testaaEtteiEttaJarjestysPysyy(){
+        Paikka eka = new Paikka(1, 1, 1);
+        Paikka toka = new Paikka(2, 2, 2);
+        
+        pino.push(eka);
+        pino.push(toka);
+        
+        pino.pop();
+        assertEquals(eka, pino.pop());
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-     @Test
-     public void hello() {}
 }
