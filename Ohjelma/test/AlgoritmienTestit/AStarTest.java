0fa3aad77852b77305e2bb3ae58cf3695ca6d61c
Cobrelli <Cobrelli@192.168.1.34>
2012-12-20 21:38:58 +0200

    testejä

--- Ohjelma/test/AlgoritmienTestit/AStarTest.java
+++ Ohjelma/test/AlgoritmienTestit/AStarTest.java
@@ -7,12 +7,9 @@ package AlgoritmienTestit;
 import Algoritmit.AStar;
 import Sokkelo.Sokkelo;
 import java.util.Random;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
@@ -37,5 +34,31 @@ public class AStarTest {
         int pituus = a.ratkaiseSokkelo(s.teeSokkelo(1, 6, 0, 5), 0, 5);
         assertEquals(5, pituus);
     }
+    
+    @Test
+    public void testaaEttaSelviaaSuurehkoistaSyotteista(){
+        a.ratkaiseSokkelo(s.teeSokkelo(500, 500, 499, 499), 499, 499);
+    }
+    
+    @Test
+    public void testaaEttaUseillaSuoritusKerroillaTestiEiKaaduMillaan(){
+        for(int i = 0;i<10;i++){
+            a.ratkaiseSokkelo(s.teeSokkelo(50, 50, 49, 49), 49, 49);
+        }
+    }
+    
+    @Test
+    public void testaaEttaReittiLoytyyMyosEiSokkeloisemmalla() {
+        s.teeSokkelo(5, 5, 4, 4);
+        
+        int pituus = a.ratkaiseSokkelo(s1, s.getLoppuY(), s.getLoppuX());
+        assertEquals(16, pituus);        
+    }
+    public static int[][] s1 = {{0, 1, 0, 0, 0},
+        {0, 1, 0, 1, 0},
+        {0, 1, 0, 1, 0},
+        {0, 1, 0, 1, 0},
+        {0, 0, 0, 1, 0}};
+    
     
 }
