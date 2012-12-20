0f257915ceb5e14852d0c67896d4620f82535026
Cobrelli <Cobrelli@192.168.1.34>
2012-12-20 20:41:15 +0200

    javadocsei

--- Ohjelma/src/Tietorakenteet/Pino.java
+++ Ohjelma/src/Tietorakenteet/Pino.java
@@ -3,7 +3,7 @@ package Tietorakenteet;
 import Algoritmit.Paikka;
 
 /**
- *
+ * Pino, jota käytetään aputietorakenteena iteratiivisessa DFS:ssä.
  * @author Cobrelli
  */
 public class Pino {
@@ -11,11 +11,19 @@ public class Pino {
     private Paikka[] alkiot;
     private int top;
 
+    /**
+     * Luo uuden pino-olion annetulla koolla sekä alustaa sen tyhjäksi.
+     * @param koko      Kertoo halutun pinon koon.
+     */
     public Pino(int koko) {
         top = -1;
         alkiot = new Paikka[koko];
     }
 
+    /**
+     * Poistaa ja palauttaa ylimmän olion pinossa.
+     * @return          Palauttaa ylimmän olion.
+     */
     public Paikka pop() {
         if (tyhja()) {
             return null;
@@ -25,6 +33,10 @@ public class Pino {
         return poistettava;
     }
 
+    /**
+     * Puskee pinoon uuden olion.
+     * @param uusi      Antaa viitteen uusimpaan lisättävään paikka olioon.
+     */
     public void push(Paikka uusi) {
         if (taysi()) {
             return;
@@ -33,10 +45,18 @@ public class Pino {
         alkiot[top] = uusi;
     }
 
+    /**
+     * Kertoo onko pino tyhjä.
+     * @return          Palauttaa true jos pino on tyhjä, muuten false.
+     */
     public boolean tyhja() {
         return top == -1;
     }
 
+    /**
+     * Kertoo onko pino taysi.
+     * @return          Palauttaa true jos pino on täysi, false jos ei.
+     */
     public boolean taysi() {
         return top == alkiot.length - 1;
     }
