7ed263277f3d4fb0da76026c79f51e40dcbc4f42
Cobrelli <Cobrelli@192.168.1.34>
2012-12-21 23:51:50 +0200

    javadocs

--- Ohjelma/test/AlgoritmienTestit/AStarTest.java
+++ Ohjelma/test/AlgoritmienTestit/AStarTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package AlgoritmienTestit;
 
 import Algoritmit.AStar;
@@ -12,7 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- *
+ * Testaa A* algoritmin toimintaa.
  * @author Cobrelli
  */
 public class AStarTest {
@@ -20,26 +16,41 @@ public class AStarTest {
     Sokkelo s;
     AStar a;
     
+    /**
+     * Konstruktori
+     */
     public AStarTest() {
     }
     
+    /**
+     * Alustaa sokkelon ja algoritmin luokkien luonnit.
+     */
     @Before
     public void setUp() {
         s = new Sokkelo(new Random());
         a = new AStar();
     }
     
+    /**
+     * Testaa että annettu reitti on lineaarisella reitillä oikean pituinen.
+     */
     @Test
     public void testaaEttaReittiOnOikeanPituinen(){
         int pituus = a.ratkaiseSokkelo(s.teeSokkelo(1, 6, 0, 5), 0, 5);
         assertEquals(5, pituus);
     }
     
+    /**
+     * Tarkistaa ettei suuren sokkelon selvittäminen ei aiheuta poikkeuksia.
+     */
     @Test
     public void testaaEttaSelviaaSuurehkoistaSyotteista(){
         a.ratkaiseSokkelo(s.teeSokkelo(500, 500, 499, 499), 499, 499);
     }
     
+    /**
+     * Tarkistaa että useallakin suorituskerralla ohjelma ei aiheuta poikkeusta.
+     */
     @Test
     public void testaaEttaUseillaSuoritusKerroillaTestiEiKaaduMillaan(){
         for(int i = 0;i<10;i++){
@@ -47,6 +58,10 @@ public class AStarTest {
         }
     }
     
+    /**
+     * Testaa että löytää perille erikseen tehdyssä labyrintissa, jossa reitti
+     * kulkee seinien ohjaamana.
+     */
     @Test
     public void testaaEttaReittiLoytyyMyosEiSokkeloisemmalla() {
         s.teeSokkelo(5, 5, 4, 4);
@@ -54,6 +69,9 @@ public class AStarTest {
         int pituus = a.ratkaiseSokkelo(s1, s.getLoppuY(), s.getLoppuX());
         assertEquals(16, pituus);        
     }
+    /**
+     * Erikseen generoitu sokkelo, jossa reitti kulkee jatkuvien seinien ohjaamana.
+     */
     public static int[][] s1 = {{0, 1, 0, 0, 0},
         {0, 1, 0, 1, 0},
         {0, 1, 0, 1, 0},
