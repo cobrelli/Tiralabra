36ba71e3afdcb61d23a026ba15636e7f43af9294
Cobrelli <Cobrelli@192.168.1.34>
2012-12-22 02:08:21 +0200

    javadocs

--- Ohjelma/test/TietorakenteidenTestit/HajautustauluTest.java
+++ Ohjelma/test/TietorakenteidenTestit/HajautustauluTest.java
@@ -10,29 +10,43 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 /**
- *
+ * Hajautustaulu tietorakenteelle olevat testit.
  * @author Cobrelli
  */
 public class HajautustauluTest {
     
     Hajautustaulu ht;
     
+    /**
+     * Konstruktori
+     */
     public HajautustauluTest() {
     }
     
+    /**
+     * Alustaa uuden hajautustaulun.
+     */
     @Before
     public void setUp() {
         ht = new Hajautustaulu(10);
     }
     
-     @Test
+     /**
+     * Tarkistaa että kun tauluun lisätään paikka olio palautetaan sama olio, get
+     * kutsulla.
+     */
+    @Test
      public void testaaEttaLisattyPalautuuTaulusta() {
          Paikka p = new Paikka(1, 1, 1);
          ht.put(p);
          assertEquals(p, ht.get(p));
      }
      
-     @Test
+     /**
+     * Testaa että hajautustauluun voidaan lisätä useita eri olioita niin että
+     * ensimmäinen lisäys löytyy kuitenkin vielä taulusta getillä.
+     */
+    @Test
      public void testaaEttaHajautustauluunVoidaanLisataUseita(){
          Paikka ennenLisayksia = new Paikka(1, 1, 1);
          ht.put(ennenLisayksia);
@@ -42,7 +56,10 @@ public class HajautustauluTest {
          assertEquals(ennenLisayksia, ht.get(ennenLisayksia));
      }
      
-     @Test
+     /**
+     * Testaa että taulusta olemattoman olion haku palauttaa null.
+     */
+    @Test
      public void testaaEttaHakuTyhjastaPalauttaaNull(){
          assertEquals(null, ht.get(new Paikka(1, 1, 1)));
      }
